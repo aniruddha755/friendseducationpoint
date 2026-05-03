63371354cfdbd8a393bf26e2c6b94fd857e24190
aniruddha755 <ani2005.net@gmail.com>
2026-05-03 13:56:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,85 @@
+// Add your documents here!
+// Make sure to fill in the correct category exactly as they are spelled on your site.
+window.documentData = [
+    // Example:
+    {
+        title: "CURRENT AFFAIRS YEARBOOK 2025_TWS Academy.pdf",
+        category: "General Knowledge",
+        dateAdded: "May 02, 2026",
+        fallbackSize: "2.3 MB",
+        link: "https://drive.google.com/file/d/1YCVGNi29nYSyL1-2OcfM4NHmqEk2PkhW/view?usp=sharing"
+    },
+
+    {
+        title: "4000+ MCQs with Solutions_compressed.pdf",
+        category: "General Knowledge",
+        dateAdded: "May 02, 2026",
+        fallbackSize: "2.3 MB",
+        link: "https://drive.google.com/file/d/1NrQz_vYGTvhyE46kzpoysqOgroYNsTFD/view?usp=sharing"
+    },
+
+
+    {
+        title: "সম্পূর্ণ_ভারতের_মধ্যযুগের_ইতিহাস_এর_সম্পূর্ণ_E_BOOK_DETAILED_NOTES+MCQ.pdf",
+        category: "History",
+        dateAdded: "May 02, 2026",
+        link: "https://drive.google.com/file/d/1Zf21EgkRInWu9YhWnISNpmj0ljSuM68D/view?usp=sharing"
+    }
+];
+
+// Add your YouTube video links here!
+window.videoData = [
+    {
+        title: "Class 11 Chemistry প্রশ্ন বিচিএা 2023  । WhatsApp number :8597348522",
+        link: "https://youtu.be/g-2IQOc2GEU?si=2qkyzWAHMNERfYE3"
+    },
+    {
+        title: "Ray & Martin physics question  bank solves 2022,Uniit - 2Knematics,Chapter -3 Motion in plane,",
+        link: "https://youtu.be/OgYpgyhAc48?si=m4GhQCG3jWNiS79z"
+    },
+    {
+        title: "Class 11 Math প্রশ্ন বিচিএা 2023",
+        link: "https://youtu.be/sEX_TOuG7ZQ?si=2ROURmzzASMRgt7b"
+    },
+    {
+        title: "Class - 11 Ray & Martin Questions Bank Chemistry Solves(pdf নিতে ফোন  করো এই নাম্বারে +918597348522)",
+        link: "https://youtu.be/oznTJBkUG68?si=kPfgi9-sdAsUfbtk"
+    },
+
+    {
+        title: "class 12 physics objective ray martin",
+        link: "https://youtu.be/fs6jsrymDGk?si=sbdkRnZg4BTet4bV"
+    },
+    {
+        title: "Class 11 physics  প্রশ্ন বিচিএা 2023",
+        link: "https://youtu.be/ujk_vGU4JEo?si=qCUJAY1BASOKogrv"
+    },
+    {
+        title: "ক্লাস 11.  কেমিস্ট্রি প্রশ্ন: বিচিত্রা এর তাপ গতিবিদ্যার ( Termodinamics )  সমস্ত প্রশ্ন এর উওর আছে।",
+        link: "https://youtu.be/vusm2Jl7Rtc?si=1VT8X9anqExwVVkD"
+    },
+    {
+        title: "ক্লাস 11 Chemistry প্রশ্ন বিচিত্রার জৈব রসায়ন চ্যাপ্টারের সমস্ত প্রশ্নের উত্তর দেওয়া আছে। part -1",
+        link: "https://youtu.be/pXK120DolS0?si=3vCHcD8u9LcYeXRW"
+    },
+    {
+        title: "class 11 রাসায়নিক বন্ধন ও আণবিক গঠন  চ্যাপ্টারের প্রশ্ন বিচিএার উওর",
+        link: "https://youtu.be/m9QHLpkmJdw?si=tWwpbVjT3xooQIqU"
+    },
+    {
+        title: "ক্লাস 11 পদার্থবিদ্যার  প্রশ্ন বিচিএা সম্পূর্ণ বই। সবাই অবশ্যই subscribe করবে।",
+        link: "https://youtu.be/4PweXKWB5Bg?si=c2dPBDdwHML6-sNO"
+    },
+    {
+        title: "বিজ্ঞান ও কুসংস্কার প্রবন্ধ রচনা।  সমস্ত ক্লাসের জন্য গুরুত্বপূর্ণ।",
+        link: "https://youtu.be/I5c9HSVIz4A?si=pdhxF_3WI9RTIRf6"
+    },
+    {
+        title: "class 12 chemistry রাসায়নিক গতিবিদ্যা প্রশ্ন বিচিএার উওর",
+        link: "https://youtu.be/KdfQrQYF3yw?si=qrCW7jHKvkSTYInU"
+    },
+    {
+        title: "ক্লাস 11 জীবনবিজ্ঞানের প্রশ্ন বিচিত্রা । সবাই অবশ্যই subscribe করবে।",
+        link: "https://youtu.be/igcTaCFucOg?si=aKpCI3Ah6ReucK9D"
+    }
+];
